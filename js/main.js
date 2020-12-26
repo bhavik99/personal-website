b7f9a7ff6f172ce593df8a6256daea3dd52b5c52
Bhavik Narang <bhavik.narang99@gmail.com>
2020-12-26 21:19:57 +0000

    About me page added in stats change to

--- js/main.js
+++ js/main.js
@@ -157,7 +157,7 @@
     var clStatCount = function() {
         
         var statSection = $(".about-stats"),
-            stats = $(".stats__count");
+            stats = $(".stats__counta");
 
         statSection.waypoint({
 
@@ -165,19 +165,55 @@
 
                 if (direction === "down") {
 
-                    stats.each(function () {
-                        var $this = $(this);
+                    var TxtRotate = function(el, toRotate, period) {
+                        this.toRotate = toRotate;
+                        this.el = el;
+                        this.loopNum = 0;
+                        this.period = parseInt(period, 10) || 2000;
+                        this.txt = '';
+                        this.tick();
+                        this.isDeleting = false;
+                    };
 
-                        $({ Counter: 0 }).animate({ Counter: $this.text() }, {
-                            duration: 4000,
-                            easing: 'swing',
-                            step: function (curValue) {
-                                $this.text(Math.ceil(curValue));
-                            }
-                        });
-                    });
+                    TxtRotate.prototype.tick = function() {
+                        var i = this.loopNum % this.toRotate.length;
+                        var fullTxt = this.toRotate[i];
 
-                } 
+                        if (this.isDeleting) {
+                            this.txt = fullTxt.substring(0, this.txt.length - 1);
+                        } else {
+                            this.txt = fullTxt.substring(0, this.txt.length + 1);
+                        }
+
+                        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+                        var that = this;
+                        var delta = 300 - Math.random() * 100;
+
+                        if (this.isDeleting) { delta /= 2; }
+
+                        if (!this.isDeleting && this.txt === fullTxt) {
+                            delta = this.period;
+                            this.isDeleting = true;
+                        } else if (this.isDeleting && this.txt === '') {
+                            this.isDeleting = false;
+                            this.loopNum++;
+                            delta = 500;
+                        }
+
+                        setTimeout(function() {
+                            that.tick();
+                        }, delta);
+                    };
+
+                    for (var i=0; i<stats.length; i++) {
+                        var toRotate = stats[i].getAttribute('data-rotate');
+                        var period = stats[i].getAttribute('data-period');
+                        if (toRotate) {
+                            new TxtRotate(stats[i], JSON.parse(toRotate), period);
+                        }
+                    }
+                }
 
                 // trigger once only
                 this.destroy();
